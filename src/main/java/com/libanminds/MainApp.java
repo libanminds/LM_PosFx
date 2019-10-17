e495586859270e28275a645d9718ed7086ff66a8
Bassel Cheaib <bassel.bc@gmail.com>
2019-10-17 11:25:01 +0300

    Initial Commit

---
+++ src/main/java/com/libanminds/MainApp.java
@@ -0,0 +1,27 @@
+package com.libanminds;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/scene.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
