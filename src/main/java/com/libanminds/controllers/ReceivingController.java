909d54e2163beb7bac6871ee551961cfa93b631c
Bassel Cheaib <bassel.bc@gmail.com>
2019-10-30 16:18:53 +0200

    Sales and Receiving

--- src/main/java/com/libanminds/controllers/ReceivingController.java
+++ src/main/java/com/libanminds/controllers/ReceivingController.java
@@ -1,13 +1,25 @@
 package com.libanminds.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReceivingController implements Initializable {
 
+    @FXML
+    private TextField itemQtty;
+
+    @FXML
+    private TextField itemDiscount;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        itemQtty.setOnMouseClicked(e -> itemQtty.selectAll());
+        itemDiscount.setOnMouseClicked(e -> itemDiscount.selectAll());
     }
+
 }
--- src/main/java/com/libanminds/controllers/SalesController.java
+++ src/main/java/com/libanminds/controllers/SalesController.java
@@ -1,13 +1,24 @@
 package com.libanminds.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SalesController implements Initializable {
 
+    @FXML
+    private TextField itemQtty;
+
+    @FXML
+    private TextField itemDiscount;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        itemQtty.setOnMouseClicked(e -> itemQtty.selectAll());
+        itemDiscount.setOnMouseClicked(e -> itemDiscount.selectAll());
     }
 }
