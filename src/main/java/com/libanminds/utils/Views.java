e2812be6803702e6b06c158c9b20b077d2411689
Bassel Cheaib <bassel.bc@gmail.com>
2019-11-03 10:49:33 +0200

    Minor refactoring

--- src/main/java/com/libanminds/utils/Views.java
+++ src/main/java/com/libanminds/utils/Views.java
@@ -1,20 +1,20 @@
 package com.libanminds.utils;
 
 public final class Views {
-    public static final String LOGIN = "/view/login.fxml";
-    public static final String MAIN = "/view/main.fxml";
-    public static final String DASHBOARD = "/view/dashboard.fxml";
-    public static final String CUSTOMERS = "/view/customers.fxml";
-    public static final String SUPPLIERS = "/view/suppliers.fxml";
-    public static final String ITEMS = "/view/items.fxml";
-    public static final String REPORTS = "/view/reports.fxml";
-    public static final String SALES = "/view/sales.fxml";
-    public static final String RECEIVING = "/view/receiving.fxml";
-    public static final String EXPENSES = "/view/expenses.fxml";
-    public static final String INCOME = "/view/income.fxml";
-    public static final String APPOINTMENTS = "/view/appointments.fxml";
-    public static final String EMPLOYEES = "/view/employees.fxml";
-    public static final String SETTINGS = "/view/settings.fxml";
+    public static final String LOGIN = "/views/login.fxml";
+    public static final String MAIN = "/views/main.fxml";
+    public static final String DASHBOARD = "/views/main/dashboard.fxml";
+    public static final String CUSTOMERS = "/views/main/customers.fxml";
+    public static final String SUPPLIERS = "/views/main/suppliers.fxml";
+    public static final String ITEMS = "/views/main/items.fxml";
+    public static final String REPORTS = "/views/main/reports.fxml";
+    public static final String SALES = "/views/main/sales.fxml";
+    public static final String RECEIVING = "/views/main/receiving.fxml";
+    public static final String EXPENSES = "/views/main/expenses.fxml";
+    public static final String INCOME = "/views/main/income.fxml";
+//    public static final String APPOINTMENTS = "/views/main/appointments.fxml";
+    public static final String EMPLOYEES = "/views/main/employees.fxml";
+    public static final String SETTINGS = "/views/main/settings.fxml";
 
     private Views(){
         throw new AssertionError("You are not allowed to instantiate this class");
