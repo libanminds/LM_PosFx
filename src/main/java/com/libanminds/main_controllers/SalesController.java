42d7a66efb0de84b2d88d357ed35db4e3fcd758d
Bassel Cheaib <bassel.bc@gmail.com>
2019-11-08 16:10:30 +0200

    Changes in sales, + cars, settings and currency

--- src/main/java/com/libanminds/main_controllers/SalesController.java
+++ src/main/java/com/libanminds/main_controllers/SalesController.java
@@ -1,8 +1,14 @@
 package com.libanminds.main_controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.libanminds.enums.Currency;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import org.controlsfx.control.CheckComboBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,15 +16,55 @@ import java.util.ResourceBundle;
 public class SalesController implements Initializable {
 
     @FXML
-    private TextField itemQtty;
-
+    private TextField customerSelection;
+    @FXML
+    private Button addCustomer;
+    @FXML
+    private CheckComboBox<?> carSelection;
+    @FXML
+    private Button addCar;
+    @FXML
+    private ComboBox<String> currencySelection;
+    @FXML
+    private ComboBox<?> modeSelection;
+    @FXML
+    private Label invoiceNb;
+    @FXML
+    private TextField itemSelection;
+    @FXML
+    private Button addItem;
+    @FXML
+    private TextField itemQuantity;
     @FXML
     private TextField itemDiscount;
-
+    @FXML
+    private TableView<?> itemsTable;
+    @FXML
+    private ComboBox<?> taxSelection;
+    @FXML
+    private TextField globalDiscount;
+    @FXML
+    private Label subtotal;
+    @FXML
+    private Label discount;
+    @FXML
+    private Label taxes;
+    @FXML
+    private Label total;
+    @FXML
+    private JFXButton clearBtn;
+    @FXML
+    private JFXButton saveBtn;
+    @FXML
+    private TextField pastInvoiceSearch;
+    @FXML
+    private TableView<?> pastInvoicesTable;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        itemQtty.setOnMouseClicked(e -> itemQtty.selectAll());
+        itemQuantity.setOnMouseClicked(e -> itemQuantity.selectAll());
         itemDiscount.setOnMouseClicked(e -> itemDiscount.selectAll());
+        currencySelection.setItems(FXCollections.observableArrayList(Currency.getSymbols()));
+        currencySelection.setValue(Currency.LBP.getSymbol());
     }
 }
